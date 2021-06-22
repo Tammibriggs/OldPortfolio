f559326a4b97c8172be92d9d20c8f8aefbc65c11
Tammibriggs <Godsfirstbriggs@gmail.com>
2021-06-22 14:43:59 +0100

    added wrapper around email

--- src/Email/Email.js
+++ src/Email/Email.js
@@ -4,19 +4,21 @@ import emailIcon from "../images/email-icon.svg"
 
 function Email(){
     return(
-        <div class="send-email">
-            <div class="email-heading mail">
-                <img src={emailIcon} class="email-icon"/>
-                <h2>drop an email</h2>
-            </div>
+        <div className="wrapper">
+            <div class="send-email">
+                <div class="email-heading mail">
+                    <img src={emailIcon} class="email-icon"/>
+                    <h2>drop an email</h2>
+                </div>
 
-            <div class="email-feilds mail">
-                <form action="#" method="POST" data-netlify="true" name="Portfolio_contact_form">
-                    <input type="text" placeholder="Name" name="name"/>
-                    <input type="email" placeholder="Your email" name="email"/>
-                    <textarea placeholder="Message" name="message"></textarea>
-                    <button type="submit">Send email</button>
-                </form>  
+                <div class="email-feilds mail">
+                    <form action="#" method="POST" data-netlify="true" name="Portfolio_contact_form">
+                        <input type="text" placeholder="Name" name="name"/>
+                        <input type="email" placeholder="Your email" name="email"/>
+                        <textarea placeholder="Message" name="message"></textarea>
+                        <button type="submit">Send email</button>
+                    </form>  
+                </div>
             </div>
         </div>
     )
